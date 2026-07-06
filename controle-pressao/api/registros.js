c849ca0c20d8442ba76f3f9f9d7e47d65c9788cd
Bruno Lima <93bruno.lima@gmail.com>
2026-07-06 14:08:30 -0300

    Ajuste Layout

--- controle-pressao/api/registros.js
+++ controle-pressao/api/registros.js
@@ -4,24 +4,25 @@ const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
 )
-console.log("URL:", process.env.SUPABASE_URL);
-console.log("KEY:", process.env.SUPABASE_KEY);
 
 export default async function handler(req, res) {
 
     const { data, error } = await supabase
         .from('registro_pressao')
-        .select('*')
+        .select('dia:id_registro, data_hora, sistolica, diastolica')
         .order('data_hora', { ascending: false });
 
     if (error) {
-
         return res.status(500).json({
             erro: error.message
         });
-
     }
 
-    return res.status(200).json(data);
+    const registros = data.map(registro => ({
+        ...registro,
+        data_hora: new Date(registro.data_hora).toLocaleDateString('pt-BR')
+    }));
+
+    return res.status(200).json(registros);
 
 }
