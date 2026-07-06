cbae483c177066e2efc1283331c1fe351fa219f0
Bruno Lima <93bruno.lima@gmail.com>
2026-07-06 14:23:47 -0300

    ajustar hora v2

--- controle-pressao/api/registros.js
+++ controle-pressao/api/registros.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
     const registros = data.map(registro => ({
         ...registro,
-        data_hora: registro.data_hora.split(',')[0]
+        data_hora: registro.data_hora.substring(0, 10)
     }));
 
     return res.status(200).json(registros);
